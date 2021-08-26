3b04e8ae6fe5b4533ad6505cb7aff8ab52f1742f
Frederik Baymler Mathiesen <frederik@baymler.com>
2021-08-26 15:26:01 +0200

    Add parameter queue and replace all parameters automatically

---
+++ bayne/container.py
@@ -0,0 +1,118 @@
+import warnings
+from collections import deque, abc
+from typing import Optional, Iterable, Iterator
+
+import torch
+from torch import nn, Tensor
+
+
+class ParameterQueue(nn.Module):
+    r"""Holds parameters in a queue.
+
+    :class:`~bayne.container.ParameterQueue`
+
+    Args:
+        cur: Current value of the parameter of :class:`~torch.Tensor`
+        parameters (iterable, optional): an iterable of :class:`~torch.Tensor` to add
+        maxlen (optional): Bound the length of the queue
+    """
+
+    def __init__(self, cur: Tensor, parameters: Optional[Iterable[Tensor]] = None, maxlen: Optional[int] = None) -> None:
+        super(ParameterQueue, self).__init__()
+        self.deque = []
+        self.maxlen = maxlen
+        self.active_index = None
+
+        self.cur = nn.Parameter(cur)
+
+        if parameters is not None:
+            self.extend(parameters)
+
+    def __len__(self) -> int:
+        return len(self.deque)
+
+    def __iter__(self) -> Iterator[Tensor]:
+        return iter(self.deque)
+
+    def __iadd__(self, parameters: Iterable[Tensor]) -> 'ParameterQueue':
+        return self.extend(parameters)
+
+    def append(self, parameter: Tensor) -> 'ParameterQueue':
+        """Appends a given tensors at the end of the list.
+
+        Args:
+            parameter (Tensor): parameter to append
+        """
+        self.deque.append(parameter)
+        self.crop()
+        return self
+
+    def crop(self):
+        self.deque = self.deque[-self.maxlen:]
+
+    def extend(self, parameters: Iterable[Tensor]) -> 'ParameterQueue':
+        """Appends tensors from a Python iterable to the end of the list.
+
+        Args:
+            parameters (iterable): iterable of parameters to append
+        """
+        if not isinstance(parameters, abc.Iterable):
+            raise TypeError("ParameterQueue.extend should be called with an "
+                            "iterable, but got " + type(parameters).__name__)
+
+        self.deque.extend(parameters)
+        self.crop()
+        return self
+
+    def save(self):
+        self.append(self.cur.data.detach().clone())
+
+    def state_dict(self, destination=None, prefix='', keep_vars=False):
+        state_dict = super().state_dict(destination, prefix, keep_vars)
+        state_dict['deque'] = list(self.deque)
+
+        return state_dict
+
+    def load_state_dict(self, state_dict, strict=True):
+        self.extend(state_dict.pop('deque'))
+
+        super().load_state_dict(state_dict, strict)
+
+    def extra_repr(self) -> str:
+        child_lines = []
+        for k, p in enumerate(self.deque):
+            size_str = 'x'.join(str(size) for size in p.size())
+            device_str = '' if not p.is_cuda else ' (GPU {})'.format(p.get_device())
+            parastr = 'Parameter containing: [{} of size {}{}]'.format(torch.typename(p), size_str, device_str)
+            child_lines.append('  (' + str(k) + '): ' + parastr)
+        tmpstr = '\n'.join(child_lines)
+        return tmpstr
+
+    def __call__(self, input):
+        raise RuntimeError('ParameterQueue should not be called.')
+
+    def _replicate_for_data_parallel(self):
+        warnings.warn("bayne.container.ParameterQueue is being used with DataParallel but this is not "
+                      "supported. This list will appear empty for the models replicated "
+                      "on each GPU except the original one.")
+
+        return super(ParameterQueue, self)._replicate_for_data_parallel()
+
+    def __torch_function__(self, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+
+        def convert_arg(arg):
+            if not isinstance(arg, ParameterQueue):
+                return arg
+
+            if arg.active_index is None:
+                return arg.cur
+
+            if 0 <= arg.active_index < len(self):
+                return arg.deque[arg.active_index]
+
+            raise ValueError('Active index out of bounds')
+
+        args = list(map(convert_arg, args))
+        return func(*args, **kwargs)
--- bayne/mcmc.py
+++ bayne/mcmc.py
@@ -5,99 +5,10 @@ import torch
 from torch import nn
 from torch.nn import Parameter
 
+from bayne.container import ParameterQueue
 from bayne.distributions import PriorWeightDistribution
 from bayne.sampler import HamiltonianMonteCarlo
 from bayne.util import ResetableModule
-
-
-def _non_copy_load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs):
-    r"""Copies parameters and buffers from :attr:`state_dict` into only
-    this module, but not its descendants. This is called on every submodule
-    in :meth:`~torch.nn.Module.load_state_dict`. Metadata saved for this
-    module in input :attr:`state_dict` is provided as :attr:`local_metadata`.
-    For state dicts without metadata, :attr:`local_metadata` is empty.
-    Subclasses can achieve class-specific backward compatible loading using
-    the version number at `local_metadata.get("version", None)`.
-
-    .. note::
-        :attr:`state_dict` is not the same object as the input
-        :attr:`state_dict` to :meth:`~torch.nn.Module.load_state_dict`. So
-        it can be modified.
-
-    Args:
-        state_dict (dict): a dict containing parameters and
-            persistent buffers.
-        prefix (str): the prefix for parameters and buffers used in this
-            module
-        local_metadata (dict): a dict containing the metadata for this module.
-            See
-        strict (bool): whether to strictly enforce that the keys in
-            :attr:`state_dict` with :attr:`prefix` match the names of
-            parameters and buffers in this module
-        missing_keys (list of str): if ``strict=True``, add missing keys to
-            this list
-        unexpected_keys (list of str): if ``strict=True``, add unexpected
-            keys to this list
-        error_msgs (list of str): error messages should be added to this
-            list, and will be reported together in
-            :meth:`~torch.nn.Module.load_state_dict`
-    """
-    for hook in self._load_state_dict_pre_hooks.values():
-        hook(state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs)
-
-    persistent_buffers = {k: v for k, v in self._buffers.items() if k not in self._non_persistent_buffers_set}
-    local_name_params = itertools.chain(self._parameters.items(), persistent_buffers.items())
-    local_state = {k: v for k, v in local_name_params if v is not None}
-
-    parameter_names = self._parameters.keys()
-    buffer_names = persistent_buffers.keys()
-
-    for name, param in local_state.items():
-        key = prefix + name
-        if key in state_dict:
-            input_param = state_dict[key]
-            # This is used to avoid copying uninitialized parameters into
-            # non-lazy modules, since they dont have the hook to do the checks
-            # in such case, it will error when accessing the .shape attribute.
-            is_param_lazy = torch.nn.parameter.is_lazy(param)
-            # Backward compatibility: loading 1-dim tensor from 0.3.* to version 0.4+
-            if not is_param_lazy and len(param.shape) == 0 and len(input_param.shape) == 1:
-                input_param = input_param[0]
-
-            if not is_param_lazy and input_param.shape != param.shape:
-                # local shape should match the one in checkpoint
-                error_msgs.append('size mismatch for {}: copying a param with shape {} from checkpoint, '
-                                  'the shape in current model is {}.'
-                                  .format(key, input_param.shape, param.shape))
-                continue
-            try:
-                if name in parameter_names:
-                    self._parameters[name] = Parameter(input_param.to(dtype=param.dtype, device=param.device))
-                elif name in buffer_names:
-                    param.copy_(input_param)
-                else:
-                    raise Exception('Could not find parameter {} on class'.format(name))
-            except Exception as ex:
-                error_msgs.append('While copying the parameter named "{}", '
-                                  'whose dimensions in the model are {} and '
-                                  'whose dimensions in the checkpoint are {}, '
-                                  'an exception occurred : {}.'
-                                  .format(key, param.size(), input_param.size(), ex.args))
-        elif strict:
-            missing_keys.append(key)
-
-    if strict:
-        for key in state_dict.keys():
-            if key.startswith(prefix):
-                input_name = key[len(prefix):]
-                input_name = input_name.split('.', 1)[0]  # get the name of param/buffer/child
-                if input_name not in self._modules and input_name not in local_state:
-                    unexpected_keys.append(key)
-
-
-def set_non_copy_load_state_dict(self):
-    self._load_from_state_dict = types.MethodType(_non_copy_load_from_state_dict, self)
-    return self
 
 
 class MonteCarloBNN(nn.Module, ResetableModule):
@@ -105,15 +16,30 @@ class MonteCarloBNN(nn.Module, ResetableModule):
         super().__init__()
 
         self.network = network
-        self.states = []
+        self.num_states = 0
         self.sampler = sampler
 
-    def sample(self, negative_log_prob, num_samples=1000, reject=0, progress_bar=True):
-        self.states = self.sampler.sample(self, negative_log_prob, num_samples, reject, progress_bar)
+        def _replace_parameter(m):
+            for name, param in list(m.named_parameters(recurse=False)):
+                del m._parameters[name]
+                queue = ParameterQueue(param, maxlen=None)
+                setattr(m, name, queue)
+        self.apply(_replace_parameter)
+
+    def sample(self, negative_log_prob, num_samples=200, reject=200, progress_bar=True):
+        def _set_maxlen(m):
+            if isinstance(m, ParameterQueue):
+                m.maxlen = num_samples
+        self.apply(_set_maxlen)
+
+        self.sampler.sample(self, negative_log_prob, num_samples, reject, progress_bar)
+        self.num_states = self.num_samples
 
     def forward(self, *args, state_idx=None, **kwargs):
-        if state_idx is not None:
-            self.load_network(state_idx)
+        def _set_active_index(m):
+            if isinstance(m, ParameterQueue):
+                m.active_index = state_idx
+        self.apply(_set_active_index)
 
         return self.network(*args, **kwargs)
 
@@ -126,24 +52,11 @@ class MonteCarloBNN(nn.Module, ResetableModule):
         preds = self.predict_dist(*args, dim=dim, **kwargs)
         return preds.mean(dim=dim)
 
-    def state_dict(self, destination=None, prefix='', keep_vars=False):
-        state_dict = super().state_dict(destination, prefix, keep_vars)
-        state_dict['states'] = self.states
-
-        return state_dict
-
-    def load_state_dict(self, state_dict, strict=True):
-        self.states = state_dict.pop('states')
-        self.load_network(0)
-
-        super().load_state_dict(state_dict, strict)
-
     def log_prior(self, dist=PriorWeightDistribution()):
         return torch.stack([dist.log_prior(w).sum() for w in self.network.parameters()]).sum()
 
-    def load_network(self, idx):
-        self.apply(set_non_copy_load_state_dict)
-        self.network.load_state_dict(self.states[idx])
-
-    def subnetwork_state_dict(self):
-        return self.network.state_dict()
+    def save(self):
+        def _save(m):
+            if isinstance(m, ParameterQueue):
+                m.save()
+        self.apply(_save)
