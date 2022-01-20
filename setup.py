e8e346cc431aa47787779ce22de45ef12be8715a
Frederik Baymler Mathiesen <frederik@baymler.com>
2022-01-20 16:39:25 +0100

    Bump minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bayne",
-    version="0.1.0",
+    version="0.1.1",
     author="Frederik Baymler Mathiesen",
     author_email="frederik@baymler.com",
     description="Bayesian Neural Networks in Pytorch",
